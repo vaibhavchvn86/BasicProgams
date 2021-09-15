f12f412ae37a83be4aeeb5bb402322a2d29a3961
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-15 23:40:41 +0530

    flip coin to get percentage of heads and tails

---
+++ FlipCoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5Assignments
+{
+    class FlipCoin
+    {
+        public static void Flip()
+        {
+            int flipcount = 10;
+            double Heads = 0;
+            double Tails = 0;
+
+
+            for (int i = 1; i <= flipcount; i++)
+            {
+                Random random = new Random();
+                int flip = random.Next(0, 2);
+                if (flip < 0.5)
+                {
+                    Console.WriteLine("Tails");
+                    Tails += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Heads");
+                    Heads += 1;
+                }
+                Console.WriteLine(+(Heads / flipcount) * 100 + "%");
+                Console.WriteLine(+(Tails / flipcount) * 100 + "%");
+            }
+        }
+    }
+}

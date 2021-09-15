aaee1c2edd20de2182b3224636632b0979e6dede
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-15 23:36:28 +0530

    even and odd numbers

---
+++ EvenOdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5Assignments
+{
+    class EvenOdd
+    {
+        public static void Num()
+        {
+            int N;
+            Console.WriteLine("Enter Number N: ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 2 == 0)
+                Console.WriteLine(+N+" is Even number");
+            else
+                Console.WriteLine(+N+" is Odd number");
+
+        }
+    }
+}

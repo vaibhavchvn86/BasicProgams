9102f78d18ec90bdcf6cb1fb2e25097bb62a5512
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-15 23:42:06 +0530

    finding power of 2 of nth number

---
+++ PowerOf2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5Assignments
+{
+    class PowerOf2
+    {
+        public static void Power()
+        {
+           double n;
+           double N;
+            Console.WriteLine("Enter number to find Power of 2*N:");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            // power of 2 only till 0<=N<31 as overflows int
+            
+            n =  Math.Pow(N, 2);
+            Console.WriteLine("Power of 2^N=" + n);
+        }
+    }
+}

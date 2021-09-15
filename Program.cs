c893c6ef849ab66b44c7b04f428fb3ad1c762186
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-15 23:44:26 +0530

    solution file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day5Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoin.Flip();
+            LeapYear.Leapyr();
+            PowerOf2.Power();
+            HarmonicNum.Harmonic();
+            Factor.fact();
+            QuotientRemainder.Num1();
+            SwapNum.Swap();
+            EvenOdd.Num();
+            VowelCosonant.Alphabet();
+            LargestNum.Large();
+        }
+    }
+}

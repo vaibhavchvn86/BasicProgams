49ba5cb0c402cf8aaee8909a4cf9115080d8d2b2
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-15 23:37:25 +0530

    checking leap year

---
+++ LeapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5Assignments
+{
+    class LeapYear
+    {
+        public static void Leapyr()
+        {
+            double Year;
+
+            Console.WriteLine("Enter Year In YYYY Format: ");
+            Year = Convert.ToDouble(Console.ReadLine());
+            if ((Year % 100) != 0 && (Year % 4) == 0)
+                Console.WriteLine("This is a Leap Year");
+            else
+                Console.WriteLine("This is not a Leap Year");
+
+        }
+    }
+}

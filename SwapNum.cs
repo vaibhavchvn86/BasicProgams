7bc57aed5f6c4f031d22e0d9c76f5dc1496821e4
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-15 23:42:54 +0530

    swaping numbers

---
+++ SwapNum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5Assignments
+{
+    class SwapNum
+    {
+        public static void Swap()
+        {
+            int val1, val2;
+            val1 = 100;
+            val2 = 200;
+
+            Console.WriteLine("Values before swap");
+            Console.WriteLine(val1.ToString());
+            Console.WriteLine(val2.ToString());
+
+            val1 = val1 + val2;
+            val2 = val1 - val2;
+            val1 = val1 - val2;
+
+            Console.WriteLine("Values after swap");
+            Console.WriteLine(val1.ToString());
+            Console.WriteLine(val2.ToString());
+            Console.ReadLine();
+        }
+    }
+    }
+

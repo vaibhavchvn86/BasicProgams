2c170a18e83fbb906b087f500fd9ea143e747615
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-15 23:45:19 +0530

    Quotient and Remainder of number

---
+++ QuotientRemainder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5Assignments
+{
+    class QuotientRemainder
+    {
+        public static void Num1()
+        {
+            
+            int remainder;
+            Console.WriteLine("Enter Dividend/Numerator: ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Divisor/Denominator: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = Math.DivRem(dividend, divisor, out remainder);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}

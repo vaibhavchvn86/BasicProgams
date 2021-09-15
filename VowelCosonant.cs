df9a82a1aafacfbdf927f703637360b9a080e3a3
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-15 23:43:58 +0530

    checking alphabet is vowel or consonant

---
+++ VowelCosonant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5Assignments
+{
+    class VowelCosonant
+    {
+        public static void Alphabet()
+        {
+            //string a;
+            //string e;
+            //string i;
+            //string o;
+            //string u;
+            Console.WriteLine("Enter any aplhabet a-z: ");
+            string Alpha = Console.ReadLine();
+          
+            if (Alpha == "a" || Alpha == "e" || Alpha == "i" || Alpha == "o" || Alpha == "u")
+                Console.WriteLine(" Aplhabet" + Alpha + " is Vowel");
+            else
+                Console.WriteLine(" Aplhabet"+Alpha +" is Consonant");
+
+        }
+
+    }
+}

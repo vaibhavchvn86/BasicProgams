7a510790138c7ffad67cdbf5de0e088691264054
Vaibhav Chavan <88843275+vaibhavchvn86@users.noreply.github.com>
2021-09-16 19:37:01 +0530

    prime factor using brute force

--- Factor.cs
+++ Factor.cs
@@ -6,61 +6,31 @@ namespace Day5Assignments
 {
     class Factor
     {
-        public static void fact()
+       public static void fact()
         {
-            //double N;
-            //int result;
-            //Console.WriteLine("Enter Number N to find its prime factors");
-            //N = Convert.ToInt32(Console.ReadLine());
-            //while (N %2 == 0)
-            //{
-            //    Console.WriteLine(2 + "");
-            //    N /= 2;
-            //    break;
-            //}
-            //for (int i=3; i*i<= N; i++)
-            //{
-            //    while (N % i == 0)
-            //        Console.WriteLine(i + "");
-            //    N /= i;
-            //    break;
-             
-            
-            
-            
-            
-            //List result = new List();
-
-            //// Take out the 2s.
-            //while (N % 2 == 0)
-            //    {
-            //        result.Add(2);
-            //        N /= 2;
-            //    }
-
-            //    // Take out other primes.
-            //    long factor = 3;
-            //    while (factor * factor <= N)
-            //    {
-            //        if (N % factor == 0)
-            //        {
-            //            // This is a factor.
-            //            result.Add(factor);
-            //            N /= factor;
-            //        }
-            //        else
-            //        {
-            //            // Go to the next odd number.
-            //            factor += 2;
-            //        }
-            //    }
-
-            //    // If num is not 1, then whatever is left is prime.
-            //    if (N > 1) result.Add(N);
-
-            //    return result;
-            
-
+            double N;
+            int i;
+            Console.WriteLine("Enter Number N to find its prime factors");
+            N = Convert.ToInt32(Console.ReadLine());
+            while (N % 2 == 0)
+            {
+                N /= 2;
+                Console.WriteLine(+2);
+                break;
+            }
+            for ( i = 3; i * i <= N; i++)
+            {
+                while (N % i == 0)
+                {
+                    N /= i;
+                    Console.WriteLine(+i);
+                }
+                break;
+            }
+            if (N > 2)
+            {
+                Console.WriteLine(+N);
+            }           
         }
     }
 }
